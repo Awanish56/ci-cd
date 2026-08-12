0eb5fa6635475be2ad606452db27587c21dbc8ba
Awanish Sharma <awanish.sharma.5683@gmail.com>
2026-08-12 19:22:27 +0530

    initiate

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+    res.send("Hello! Node.js CI/CD Pipeline is working 🚀");
+});
+
+app.get("/health", (req, res) => {
+    res.json({
+        status: "UP"
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
